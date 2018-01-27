b33e492065f16cd1e7aaf4d2eb1909b4cc98ea53
sharkinsspatial <sharkinsgis@gmail.com>
2018-01-27 16:51:03 -0300

    Container tests.

--- test/Container_test.js
+++ test/Container_test.js
@@ -1,5 +1,8 @@
+/* global window */
+/* eslint-disable func-names */
 import test from 'tape';
 import React from 'react';
+import sinon from 'sinon';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { Container } from '../src/components/Container';
@@ -13,8 +16,8 @@ configure({
 
 const setup = (isAuthenticated) => {
   const exportImage = () => {};
-  const checkTokenStatus = () => {};
-  const setDronedeployAPI = () => {};
+  const checkTokenStatus = sinon.spy();
+  const setDronedeployAPI = sinon.spy();
   const props = {
     checkTokenStatus,
     exportImage,
@@ -22,12 +25,38 @@ const setup = (isAuthenticated) => {
     isAuthenticated
   };
   const wrapper = shallow(<Container {...props} />);
-  return wrapper;
+  return { wrapper, checkTokenStatus, setDronedeployAPI };
 };
 
-test('shallow', (t) => {
-  const wrapper = setup(false);
-  t.ok(wrapper.contains(<Facebook />));
-  t.notOk(wrapper.contains(<Form exportImage={() => {}} />));
+test('Container', (t) => {
+  const { wrapper } = setup(false);
+  t.equals(wrapper.find(Facebook).length, 1);
+  t.equals(wrapper.find(Form).length, 0);
   t.end();
-});
+}, 'Renders the Facebook login when the user is not authenticated');
+
+test('Container', (t) => {
+  const { wrapper } = setup(true);
+  t.equals(wrapper.find(Form).length, 1);
+  t.equals(wrapper.find(Facebook).length, 0);
+  t.end();
+}, 'Renders the Form when the user is authenticated');
+
+test('Container', (t) => {
+  const api = 'api';
+  let dronedeploy;
+  window.DroneDeploy = function () {
+    dronedeploy = Promise.resolve(api);
+    return dronedeploy;
+  };
+  configure({
+    adapter: new Adapter(),
+    disableLifecycleMethods: false
+  });
+  const { checkTokenStatus, setDronedeployAPI } = setup(true);
+  t.equal(checkTokenStatus.callCount, 1);
+  dronedeploy.then(() => {
+    t.ok(setDronedeployAPI.calledWith(api));
+    t.end();
+  });
+}, 'componentDidMount calls checkTokenStatus and creates new DroneDeploy API');
